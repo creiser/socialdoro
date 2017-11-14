0eb169df9b071b266397becfe658d99dc1754509
creiser <christianreiser@gmx.net>
2017-11-14 21:47:18 +0100

    Simulate multiple users.

--- server.py
+++ server.py
@@ -7,18 +7,28 @@ app = Flask(__name__)
 #    pomodoro_status = request.args.get('pomodoro_status', 0, type=int)
 
 debug_start = time.time() # seconds since 1970
-pomodoros = []
+num_users = 4
+pomodoros = [[] for x in range(num_users)]
 
 @app.route('/_add_pomodoro')
 def add_pomodoro():
+    user_id = request.args.get('user_id', type=int)
     pomodoro_start = request.args.get('start', type=float)
     pomodoro_end = request.args.get('end', type=float)
-    pomodoros.append((pomodoro_start, pomodoro_end))
+    pomodoros[user_id].append(pomodoro_start)
+    pomodoros[user_id].append(pomodoro_end)
     print(pomodoros)
     return jsonify(result="Hello World!")
 
+@app.route('/_get_pomodoros')
+def get_pomodoros():
+    return jsonify(pomodoros=pomodoros)
+
 @app.route('/')
 def index():
+    user_id = request.args.get('user_id', 0, type=int)
     return render_template('index.html',
+                           user_id=user_id,
+                           num_users=num_users,
                            debug_start=debug_start,
-                           pomodoros=list(sum(pomodoros, ())))
+                           pomodoros=pomodoros)
