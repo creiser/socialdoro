3892498c802eb6034e6723a47244131d5fba11da
creiser <christianreiser@gmx.net>
2017-11-15 18:29:01 +0100

    Synchronize pomodoros between server and client. Not finished yet. Need to account for sync button.

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ num_users = 4
 users = [{'pomodoro_state' : POMODOROSTATE_OFFLINE,
           'pomodoro_start' : None,
           'last_update' : 0,
-          'pomodoros' : []} for _ in range(num_users)]
+          'pomodoros' : []} for _ in range(num_users)] # [start, end, start, end, ..]
 
 def check_for_timeouts():
     # TODO: in case of timeout we could nevertheless add pomodoro
@@ -27,7 +27,17 @@ def user_status():
     users[user_id]['pomodoro_state'] = request.args.get('pomodoro_state', 0, type=int)
     users[user_id]['pomodoro_start'] = request.args.get('pomodoro_start', 0, type=float)
     users[user_id]['last_update'] = time.time()
-    check_for_timeouts()
+
+    # Convert comma seperated string into float array
+    # Make sure to not overwrite pomodoros saved on server
+    # with empty pomodoro array that we get on first call
+    retrieved_pomodoros = request.args.get('pomodoros', '', type=str)
+    if retrieved_pomodoros:
+        users[user_id]['pomodoros'] = [float(x) for x in retrieved_pomodoros.split(',')]
+
+    print(users[user_id]['pomodoros'])
+
+    check_for_timeouts() # TODO: put this check somewhere else, just a hack
     return jsonify(users=users)
 
 @app.route('/_add_pomodoro')
@@ -35,14 +45,12 @@ def add_pomodoro():
     user_id = request.args.get('user_id', type=int)
     pomodoro_start = request.args.get('start', type=float)
     pomodoro_end = request.args.get('end', type=float)
+    users[user_id]['pomodoros'].append(pomodoro_start)
+    users[user_id]['pomodoros'].append(pomodoro_end)
     #pomodoros[user_id].append(pomodoro_start)
     #pomodoros[user_id].append(pomodoro_end)
     #print(pomodoros)
-    return jsonify(result="Hello World!")
-
-#@app.route('/_get_pomodoros')
-#def get_pomodoros():
-#    return jsonify(pomodoros=pomodoros)
+    return jsonify(result="")
 
 @app.route('/')
 def index():
