676849e833ec8cd8e08fc817ef7a94d87b2584e4
creiser <christianreiser@gmx.net>
2017-11-19 14:33:23 +0100

    Pomodoro Personal polished with text in middle. (Crazy hack)

--- app/src/components/PomodoroPersonal.js
+++ app/src/components/PomodoroPersonal.js
@@ -4,8 +4,9 @@
 import React, {Component} from 'react';
 
 import { Line, Circle } from 'rc-progress';
-import { PomodoroState, get_rel_time, pomodoro_time, break_time, getUserProgressInPercent } from '../Util';
+import { PomodoroState, get_rel_time, pomodoro_time, break_time, getUserProgressInPercent, prettyTime } from '../Util';
 import { Button } from 'react-bootstrap';
+import $ from 'jquery';
 
 class PomodoroPersonal extends Component {
     render() {
@@ -16,26 +17,45 @@ class PomodoroPersonal extends Component {
 		var width = getUserProgressInPercent(this.props.users[this.props.user_id]);
 		
 		// Current user specific
-        var info_text = 'Press start to start pomodoro';
+        //var info_text = 'Press start to start pomodoro';
+		var info_text = '';
+		var remaining_time = 'Paused';
 
         var current_user = this.props.users[this.props.user_id];
         if (current_user.pomodoro_state == PomodoroState.POMODORO) {
-            info_text = "Work! Next break in " + Math.round(current_user.pomodoro_start + pomodoro_time - get_rel_time()) + " seconds.";
+            //info_text = "Work! Next break in " + Math.round(current_user.pomodoro_start + pomodoro_time - get_rel_time()) + " seconds.";
+			remaining_time = prettyTime(current_user.pomodoro_start + pomodoro_time - get_rel_time());
         } else if (current_user.pomodoro_state == PomodoroState.BREAK) {
-            info_text = "Take a break! Next pomodoro in " + Math.round(current_user.pomodoro_start + pomodoro_time + break_time - get_rel_time()) + " seconds.";
+            //info_text = "Take a break! Next pomodoro in " + Math.round(current_user.pomodoro_start + pomodoro_time + break_time - get_rel_time()) + " seconds.";
+			remaining_time = prettyTime(current_user.pomodoro_start + pomodoro_time + break_time - get_rel_time());
         }
 
         var control_button_text = current_user.pomodoro_state == PomodoroState.POMODORO ||
         current_user.pomodoro_state == PomodoroState.BREAK ? 'Stop' : 'Start';
 		
+		// The biggest hack of all time, just hack in, don't care, I love it
+		//$('#personalCountdown').remove();
+		//$('.rc-progress-circle ').append('<text id="personalCountdown" x="50%" y="50%" fill="black" text-anchor="middle" alignment-baseline="central" font-size="10">' + remaining_time + '</text>');
+		
+		// candidate for hacking hall of fame
+		$('#rem_t').html('<svg viewBox="0 0 100 100"><text x="50%" y="50%" fill="black" text-anchor="middle" alignment-baseline="central" font-size="13">' + remaining_time + '</text></svg>');
+		var offset = $('.rc-progress-circle').outerHeight();
+		
         return (
-            <div className="pomodoro_personal">
+            <div className="pomodoro_personal" style={{textAlign: 'center'}}>
 				<Circle percent={width} strokeWidth="4" trailWidth="4" strokeColor={stroke_color} />
+				<div id="rem_t" style={{marginBottom: -offset, position: 'relative', bottom: offset}}>
+				</div>
 				<Button onClick={this.props.onControlClick} bsStyle="primary">{control_button_text}</Button>
                 <div>{info_text}</div>
             </div>
         );
     }
+	
+	componentDidMount() {
+		$('#personalCountdown').remove();
+		$('.rc-progress-circle').html($('.rc-progress-circle').html() + '<text id="personalCountdown" x="50%" y="50%" fill="black" text-anchor="middle" alignment-baseline="central" font-size="10">what the fork</text>');
+	}
 }
 
 export default PomodoroPersonal;
